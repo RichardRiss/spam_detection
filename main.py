a01e849102d4434cd81fe51d50a5d01d37b41bdb
Richard Riss <rihhime@gmail.com>
2023-05-17 23:23:18 +0200

    knn added

--- main.py
+++ main.py
@@ -3,17 +3,92 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-import sklearn
+import re
+from nltk.corpus import stopwords
+#nltk.download('stopwords')
+from email.parser import BytesParser
+from email.policy import default
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+############################
+# Train data
+############################
+def train(data: pd.DataFrame):
+  # Split into features and label
+  X = data.content
+  y = data.label.astype(int)
+  
+  # Turn wordlist into tokens
+  vector = CountVectorizer()
+  X = vector.fit_transform(X)
+  X.toarray().shape
+  
+  # split into training and test data
+  X_train,X_rem,y_train,y_rem = train_test_split(X, y, test_size=0.20, random_state=39)
+  X_val,X_test,y_val,y_test = train_test_split(X_rem, y_rem, test_size=0.50, random_state=39)
+
+  # KNN model
+  knn = KNeighborsClassifier()
+  model = knn.fit(X_train, y_train)
+  y_pred = model.predict(X_val)
+  logging.info(f'"Accuracy for validation set: {(accuracy_score(y_val, y_pred)*100)}%')
+
+
+
+############################
+# Data Preprocessing
+############################
+
+filter = '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~'
+stop_words = stopwords.words('english')
+def clean_text(email):
+    text = re.sub('[{}]'.format(filter),'',email)
+    text = re.sub('\n+','',text)
+    text = re.sub('\\s+',' ',text)
+    text = re.sub(r'[0-9]+','number',text)
+    text = text.lower()
+    text = re.sub(r'[a-z0-9]{12,}','',text)
+    text = re.sub('\\s+',' ',text)
+    return text
+
+
+def stopword(text):
+    return ' '.join([w for w in text.split() if w not in stop_words])
+
+
+def dataPreprocessing(data: pd.DataFrame):
+  data['content'] = None
+  for i in range(data.shape[0]):
+    email = BytesParser(policy=default).parsebytes(data['email'][i].encode('utf-8'))
+    payload = email.get_payload()
+    data['content'][i] = stopword(clean_text(str(payload)))
+  
+  data = data.dropna(axis=0)
+  data = data.reset_index(drop=True)
+  return data
+
+
+############################
+# Import Data
+############################
 
 def importData():
   spam_ham = pd.read_csv('./Spam_Ham_data.csv')
   spam = spam_ham.loc[spam_ham.label == 1.0]
   ham = spam_ham.loc[spam_ham.label == 0]
   logging.info(f'{len(spam)} Spam sets and {len(ham)} valid sets.')
+  data = pd.concat([ham[['email','label']], spam[['email','label']]], axis=0, ignore_index=True)
+  return data
 
 
 def main():
-    importData()
+    data = importData()
+    clean_data = dataPreprocessing(data)
+    train(clean_data)
+
 
 def init_logging():
   log_format = f"%(asctime)s [%(processName)s] [%(name)s] [%(levelname)s] %(message)s"
